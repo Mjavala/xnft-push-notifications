ba078a3ff2824a0a7aff532531789a3ad167e741
k <mja>
2023-05-22 14:57:06 -0700

    add working endpoints

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,12 @@ const RPC = process.env.RPC
 const XNFT_PROGRAM_ID = process.env.XNFT_PROGRAM_ID
 const MINT = process.env.MINT
 const TITLE = process.env.TITLE
-const DESCRIPTION = process.env.DESCRIPTION
+const MESSAGE = process.env.MESSAGE
+const USER_INFO_ENDPOINT = process.env.USER_INFO_ENDPOINT
+const PUSH_NOTIFICATION_ENDPOINT = process.env.PUSH_NOTIFICATION_ENDPOINT
+const SECRET = process.env.SECRET
 
-export const getXnftHolders = async () => {
+const getXnftHolders = async () => {
   if (!RPC) {
     console.log('RPC is not defined')
     return
@@ -47,10 +50,71 @@ export const getXnftHolders = async () => {
     },
   ]
 
-  return await connection.getParsedProgramAccounts(
-    new PublicKey(XNFT_PROGRAM_ID),
-    {
-      filters,
-    }
-  )
+  return await connection.getProgramAccounts(new PublicKey(XNFT_PROGRAM_ID), {
+    filters,
+  })
 }
+
+const getUserInfo = async (user: PublicKey) => {
+  if (!USER_INFO_ENDPOINT) {
+    console.log('USER_INFO_ENDPOINT is not defined')
+    return
+  }
+  const response = await fetch(
+    `${USER_INFO_ENDPOINT}?publicKey=${user.toBase58()}`
+  )
+  if (response.status === 200) {
+    return await response.json()
+  } else {
+    console.log(
+      `Error fetching user info: ${user.toBase58()}, ${response.status}`
+    )
+    return
+    // write to cache
+  }
+}
+
+async function push(userIds: string[]) {
+  if (!PUSH_NOTIFICATION_ENDPOINT) {
+    console.log('USER_INFO_ENDPOINT is not defined')
+    return
+  }
+
+  if (!SECRET) {
+    console.log('SECRET is not defined')
+    return
+  }
+
+  const response = await fetch(PUSH_NOTIFICATION_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `secret ${SECRET}`,
+    },
+    body: JSON.stringify({
+      userIds,
+      TITLE,
+      MESSAGE,
+    }),
+  })
+
+  if (response.status === 200) {
+    console.log('Push notification sent successfully')
+  } else {
+    console.log('Error sending push notification')
+  }
+}
+
+export const run = async () => {
+  const holders = await getXnftHolders()
+  const userIds = await holders?.map(async (holder) => {
+    const dataBuffer = holder.account.data as Buffer
+    const userInfo = await getUserInfo(
+      new PublicKey(dataBuffer.slice(8, 8 + 32))
+    )
+    return userInfo?.user?.id
+  })
+  // push notification
+  // await push(userIds)
+}
+
+run()
